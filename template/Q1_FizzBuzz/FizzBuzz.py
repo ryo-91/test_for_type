27259a5b57de0e5ce97190329292ce9246a01a94
ryo-91 <ryo.linux129.3@gmail.com>
2022-05-25 22:29:13 +0900

    Q1

--- template/Q1_FizzBuzz/FizzBuzz.py
+++ template/Q1_FizzBuzz/FizzBuzz.py
@@ -3,6 +3,14 @@ for num in range(1, 101):
 
     # ここから記述
 
+    if num % 15 == 0:
+        string += 'Fizzbuss'
+    elif num % 3 == 0:
+        string += 'Fizz'
+    elif num % 5 == 0:
+        string += 'Buzz'
+    else:
+        string += str(num)
 
     # ここまで記述
 
