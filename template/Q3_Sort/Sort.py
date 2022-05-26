d79233b608a02e23f9655b95fdea962c2efc94a1
ryo-91 <ryo.linux129.3@gmail.com>
2022-05-26 17:43:08 +0900

    Q3

--- template/Q3_Sort/Sort.py
+++ template/Q3_Sort/Sort.py
@@ -1,3 +1,6 @@
+from openpyxl import NUMPY
+
+
 def main():
     #ランダムに並べられた重複のない整数の配列
     array = [5, 4, 6, 2, 1, 9, 8, 3, 7, 10]
@@ -16,6 +19,54 @@ def sort(array):
 
     # ここから記述
 
+    pivot_count = 0
+    left = []
+    right = []
+    pivot_index = 0
+    
+    for i,j in enumerate(array):
+        if j >= pivot:
+            pivot = i
+            pivot_index = i
+            for index,num in enumerate(reversed(array)):
+                if i+1 == index:
+                    break
+
+                elif num < pivot:
+                    array[pivot_index], array[-(index+1)], = array[-(index+1)], array[pivot_index]
+                    break
+        else:
+            left = array[:i]
+            right = array[index:]
+            pivot_count += 1
+            continue
+    left = sort(left)
+    right = sort(right)
+
+    return left + [pivot] * pivot_count + right
+
+#    for index,num in enumerate(reversed(array)):
+#        if num < pivot:
+#            array[pivot_index], array[-index], = array[-index], array[pivot_index]
+#            for i,j in enumerate(array):
+#                if j >= pivot:
+#                    pivot = i
+#                    pivot_index = i
+#                    break
+            
+
+
+#    for num in array:
+#        if num < pivot:
+#            left.append(num)
+#        elif num > pivot:
+#            right.append(num)
+#        else:
+#            pivot_count += 1
+#    left = sort(left)
+#    right = sort(right)
+#
+#    return left + [pivot] * pivot_count + right
 
     # ここまで記述
 
