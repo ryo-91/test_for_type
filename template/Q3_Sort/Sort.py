c700a475623b71c63318a18bd14123790676463b
ryo-91 <ryo.linux129.3@gmail.com>
2022-05-26 20:17:40 +0900

    update

--- template/Q3_Sort/Sort.py
+++ template/Q3_Sort/Sort.py
@@ -1,3 +1,4 @@
+from re import S
 from openpyxl import NUMPY
 
 
@@ -12,6 +13,8 @@ def main():
 def sort(array):
     # 要素が一つの場合はソートの必要がないので、そのまま返却
     if len(array) == 1:
+    #    print(array)
+    #    print("array")
         return array
 
     # 配列の先頭を基準値とする
@@ -19,32 +22,68 @@ def sort(array):
 
     # ここから記述
 
+
     pivot_count = 0
     left = []
     right = []
     pivot_index = 0
-    
+    array_index = list(range(len(array)))[::-1]
+    flag=0
     for i,j in enumerate(array):
-        if j >= pivot:
-            pivot = i
-            pivot_index = i
-            for index,num in enumerate(reversed(array)):
-                if i+1 == index:
-                    break
+        for index,num in enumerate(reversed(array)):
+            if i+1 == array_index[index]:
+                flag = 1
+                break
 
-                elif num < pivot:
-                    array[pivot_index], array[-(index+1)], = array[-(index+1)], array[pivot_index]
+            elif j >= pivot:
+                pivot2 = j
+                pivot2_index = i
+                if num < pivot:
+                    array[pivot2_index], array[array_index[index]], = array[array_index[index]], array[pivot2_index]
                     break
+            
+            elif num < pivot:
+                array[pivot2_index], array[array_index[index]], = array[array_index[index]], array[pivot2_index]
+                break
         else:
-            left = array[:i]
-            right = array[index:]
-            pivot_count += 1
             continue
+        left = array[:i+1]
+        right = array[i+1:] 
+        pivot_count += 1
+        if flag:
+            break
+    #print("left")
+    #print(left, right)
+
     left = sort(left)
     right = sort(right)
 
-    return left + [pivot] * pivot_count + right
+    return left  + right
+       
 
+
+#    for i,j in enumerate(array):
+#        if j >= pivot:
+#            pivot2 = j
+#            pivot2_index = i
+#            for index,num in enumerate(reversed(array)):
+#                if i+1 == array_index[index]:
+#                    break
+#
+#                elif num < pivot2:
+#                    array[pivot2_index], array[array_index[index]], = array[array_index[index]], array[pivot_index]
+#                    break
+#        else:
+#            left = array[:i]
+#            right = array[array_index[index]:]
+#            pivot_count += 1
+#            continue
+
+#    left = sort(left)
+#    right = sort(right)
+#
+#    return left + right
+#
 #    for index,num in enumerate(reversed(array)):
 #        if num < pivot:
 #            array[pivot_index], array[-index], = array[-index], array[pivot_index]
